f145b9930ed15a909760c36d04b7ce349da5f4e1
Rodrigo Escamilla <roescamilla8@gmail.com>
2022-07-14 11:37:49 -0500

    Mas cerca del resultado

--- permisoMedio.py
+++ permisoMedio.py
@@ -4,12 +4,6 @@ datos = pd.read_csv("baseClientesHackaton2022.csv")
 df = pd.DataFrame(datos)
 #print(df.head(5))
 
-import pandas as pd
-
-datos = pd.read_csv("baseClientesHackaton2022.csv")
-df = pd.DataFrame(datos)
-#print(df.head(5))
-
 def permisoIntermedio(idCliente):
     df = pd.read_csv("./baseClientesHackaton2022.csv",index_col="idCliente",parse_dates=True)
     #idCliente = 'BF000002999'
@@ -20,17 +14,21 @@ def permisoIntermedio(idCliente):
             #print(c)
         if(c==1) or (c==2) or (c==3) or (c==6) or (c==7) or (c==8) or (c==9) or (c==11) or (c==12):
             palabra = df2[c]
-            print("palabra:", palabra)
+            #print("palabra:", palabra)
             count=0
             for char in df2[c]:
                 #print("char", char)
                 if count>2:
-                    df2[c][count].replace(char, "*")
-                    count=count+1
+                    df2[c]=df2[c].replace(df2[c][count], "*")
+                    #char="*"
+                count=count+1
 
-        print ("final:", df2)
+    print ("final:", df2)
 
     # except:
     #     print("Usuario no encontrado")
 
     return
+
+permisoIntermedio('BF000002999')
+
